19d50747996f420b99d4ed609cbdcabbb08a77e5
WarrenHawker <hawker.warren@gmail.com>
2023-03-12 10:23:30 +0000

    finished adding photos and implemented image focus feature

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,5 @@
 "use strict";
+// navbar functions
 const navToggle = document.getElementById('nav-toggle');
 const sideNav = document.getElementById('nav-links');
 const navLinkButtons = document.querySelectorAll('#nav-links a');
@@ -7,6 +8,27 @@ const toggleNavMobile = () => {
 };
 navToggle.addEventListener('click', toggleNavMobile);
 navLinkButtons.forEach((button) => {
-    console.log('yes');
     button.addEventListener('click', toggleNavMobile);
+});
+// image focus functions
+const images = document.querySelectorAll('img');
+const focusedImageOverlay = (document.getElementById('image-focus-overlay'));
+const focusedImage = document.getElementById('focused-image');
+const focusOnImage = (e) => {
+    console.log('no');
+    const clickedImage = e.target;
+    focusedImageOverlay.classList.add('active');
+    focusedImage.src = clickedImage.src;
+    focusedImage.alt = clickedImage.alt;
+};
+images.forEach((image) => {
+    image.addEventListener('click', focusOnImage);
+});
+//close image focus overlay
+const closeButton = document.getElementById('overlay-close');
+closeButton.addEventListener('click', () => {
+    console.log('yes');
+    focusedImageOverlay.classList.remove('active');
+    focusedImage.src = '';
+    focusedImage.alt = '';
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+// navbar functions
 const navToggle = <HTMLElement>document.getElementById('nav-toggle');
 const sideNav = <HTMLElement>document.getElementById('nav-links');
 const navLinkButtons: NodeListOf<Element> =
@@ -11,4 +12,33 @@ navToggle.addEventListener('click', toggleNavMobile);
 
 navLinkButtons.forEach((button) => {
   button.addEventListener('click', toggleNavMobile);
+});
+
+// image focus functions
+const images: NodeListOf<Element> = document.querySelectorAll('img');
+const focusedImageOverlay = <HTMLElement>(
+  document.getElementById('image-focus-overlay')
+);
+const focusedImage = <HTMLImageElement>document.getElementById('focused-image');
+
+const focusOnImage = (e: any) => {
+  console.log('no');
+  const clickedImage = <HTMLImageElement>e.target;
+  focusedImageOverlay.classList.add('active');
+  focusedImage.src = clickedImage.src;
+  focusedImage.alt = clickedImage.alt;
+};
+
+images.forEach((image) => {
+  image.addEventListener('click', focusOnImage);
+});
+
+//close image focus overlay
+const closeButton = <HTMLElement>document.getElementById('overlay-close');
+
+closeButton.addEventListener('click', () => {
+  console.log('yes');
+  focusedImageOverlay.classList.remove('active');
+  focusedImage.src = '';
+  focusedImage.alt = '';
 });
